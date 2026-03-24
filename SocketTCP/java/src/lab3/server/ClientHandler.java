3950d3d71165f333cb378981b61623a335ec67b7
hahuy1708 <hahuy17082005@gmail.com>
2026-03-24 19:21:11 +0700

    add userId for better identify when chat

--- SocketTCP/java/src/lab3/server/ClientHandler.java
+++ SocketTCP/java/src/lab3/server/ClientHandler.java
@@ -8,11 +8,13 @@ public class ClientHandler extends Thread {
     private Socket socket;
     private JTextArea chatArea;
     private PrintWriter out;
+    private String clientID; // id random
 
     public ClientHandler(Socket s, JTextArea area) {
         this.socket = s;
         this.chatArea = area;
-    }
+        this.clientID = "User " + (1000 + (int)(Math.random() * 9000));
+       }
 
     public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
@@ -21,13 +23,15 @@ public class ClientHandler extends Thread {
                 ServerGUI.clientWriters.add(this.out);
             }
             
+            chatArea.append(clientID + " đã kết nối \n");
             String msg;
             while ((msg = in.readLine()) != null) {
-            	chatArea.append("Client: " + msg + "\n");
+            	String Msg = clientID + ": " + msg;
+            	chatArea.append(Msg + "\n");
             	ServerGUI.broadcast(msg, this.out);
             }
         } catch (IOException e) {
-        	chatArea.append("Một kết nối đã đóng.\n");
+        	chatArea.append(clientID + " kết nối đã đóng.\n");
         } finally {
             if (out != null) {
                 synchronized (ServerGUI.clientWriters) { ServerGUI.clientWriters.remove(out); }
